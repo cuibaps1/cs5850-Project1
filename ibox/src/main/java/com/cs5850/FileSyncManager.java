9d612081dbd721808d4cfcc6a28d8f247987e860
cuibaps1 <jadanngo@gmail.com>
2019-02-28 02:27:29 -0800

    update credential, creating some more classes

--- ibox/src/main/java/com/cs5850/FileSyncManager.java
+++ ibox/src/main/java/com/cs5850/FileSyncManager.java
@@ -5,9 +5,9 @@ import java.io.IOException;
 
 public interface FileSyncManager {
 
-	public void addFile(File localFile) throws IOException;
+	public void upload(File localFile) throws IOException;
 	
-	public void updateFile(File localFile) throws IOException;
+	public void modifyFile(File localFile) throws IOException;
 	
 	public void deleteFile(File localFile) throws IOException;
 	
---
+++ ibox/src/main/java/com/cs5850/WatchFolder.java
@@ -0,0 +1,5 @@
+package com.cs5850;
+
+public class WatchFolder {
+
+}
