2ae0767fc812556653ef0fbca6cb5c925ec821d7
cuibaps1 <jadanngo@gmail.com>
2019-02-28 01:29:15 -0800

    interface FileManager class

---
+++ ibox/src/main/java/com/cs5850/FileSyncManager.java
@@ -0,0 +1,15 @@
+package com.cs5850;
+
+import java.io.File;
+import java.io.IOException;
+
+public interface FileSyncManager {
+
+	public void addFile(File localFile) throws IOException;
+	
+	public void updateFile(File localFile) throws IOException;
+	
+	public void deleteFile(File localFile) throws IOException;
+	
+	
+}
